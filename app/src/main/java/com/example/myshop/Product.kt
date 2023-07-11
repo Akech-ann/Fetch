3435c4ecde4793ecf384dfeed24fc69359488c2d
Student <Akech-ann>
2023-07-11 18:29:28 +0300

    first commit

---
+++ app/src/main/java/com/example/myshop/Product.kt
@@ -0,0 +1,13 @@
+package com.example.myshop
+
+data class Product(
+    var id :Int,
+    var title :String,
+    var description :String,
+    var price:Double,
+    var ratings:Double,
+    var stock:Int,
+    var brand:String,
+    var string: String,
+    var thumbnail:String
+)
---
+++ app/src/main/java/com/example/myshop/ProductResponse.kt
@@ -0,0 +1,9 @@
+package com.example.myshop
+
+data class ProductResponse(
+    var products : List<Product>,
+    var total : Int,
+    var skip :Int,
+    var limit:Int,
+
+)
